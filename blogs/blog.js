3b3929cfd6bd12075902a861504e3541eaca1f83
机械仔 <tianbot@ros2go>
2026-01-25 18:35:27 +0800

    posts/assets/图片路径修正 v2.5

--- blogs/blog.js
+++ blogs/blog.js
@@ -642,9 +642,9 @@ function showPostDetail(post) {
             htmlContent = bodyMatch[1];
         }
 
-        // 修正图片路径：assets/ -> posts/assets/
-        // 这样无论在哪个页面访问，路径都正确
-        htmlContent = htmlContent.replace(/src="assets\//g, 'src="posts/assets/');
+        // 修正图片路径：assets/ -> /posts/assets/
+        // 使用绝对路径，无论在哪个页面访问都正确
+        htmlContent = htmlContent.replace(/src="assets\//g, 'src="/posts/assets/');
 
         // 提取已有的标题和元信息（如果HTML中有）
         const existingTitle = htmlContent.match(/<h1[^>]*>(.+?)<\/h1>/i);
